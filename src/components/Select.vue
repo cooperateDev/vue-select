73a9b1dd18f95a6261f61833a72189a862268ffb
cooperateDev <pavelasanov1206@gmail.com>
2016-03-16 10:56:38 -0700

    - search input is 100% width when value is empty

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -104,7 +104,7 @@
 </style>
 
 <template>
-  <div class="dropdown" :class="cssClasses">
+  <div class="dropdown" :class="dropdownClasses">
     <div v-el:toggle @mousedown.prevent="toggleDropdown" class="dropdown-toggle clearfix" type="button">
         <span class="form-control" v-if="!searchable && isValueEmpty">
           {{ placeholder }}
@@ -121,16 +121,17 @@
           v-el:search
           v-show="searchable"
           v-model="search"
-          @keydown.delete="maybeDeleteValue"
-          @keydown.esc="onEscape"
-          @keydown.up.prevent="typeAheadUp"
-          @keydown.down.prevent="typeAheadDown"
-          @keydown.enter.prevent="typeAheadSelect"
+          @keyup.delete="maybeDeleteValue"
+          @keyup.esc="onEscape"
+          @keyup.up.prevent="typeAheadUp"
+          @keyup.down.prevent="typeAheadDown"
+          @keyup.enter.prevent="typeAheadSelect"
           @blur="open = false"
           @focus="open = true"
           type="search"
           class="form-control"
           :placeholder="searchPlaceholder"
+          :style="{ width: isValueEmpty ? '100%' : 'auto' }"
         >
 
         <i v-el:open-indicator role="presentation" class="open-indicator glyphicon-chevron-down glyphicon"></i>
@@ -152,38 +153,104 @@
 <script>
   export default {
     props: {
+      /**
+       * Contains the currently selected value. Very similar to a
+       * `value` attribute on an <input>. In most cases, you'll want
+       * to set this as a two-way binding, using :value.sync. However,
+       * this will not work with Vuex, in which case you'll need to use
+       * the onChange callback property.
+       * @type {Object||String||null}
+       */
       value: {
-        twoway: true,
-        required: true
+        default: null
       },
+
+      /**
+       * An array of strings or objects to be used as dropdown choices.
+       * If you are using an array of objects, vue-select will look for
+       * a `label` key (ex. [{label: 'This is Foo', value: 'foo'}]). A
+       * custom label key can be set with the `label` prop.
+       * @type {Object}
+       */
       options: {
         type: Array,
         default() { return [] },
       },
+
+      /**
+       * Sets the max-height property on the dropdown list.
+       * @deprecated
+       * @type {String}
+       */
       maxHeight: {
         type: String,
         default: '400px'
       },
+
+      /**
+       * Enable/disable filtering the options.
+       * @type {Boolean}
+       */
       searchable: {
         type: Boolean,
         default: true
       },
+
+      /**
+       * Equivalent to the `multiple` attribute on a `<select>` input.
+       * @type {Object}
+       */
       multiple: {
         type: Boolean,
         default: false
       },
+
+      /**
+       * Equivalent to the `placeholder` attribute on an `<input>`.
+       * @type {Object}
+       */
       placeholder: {
         type: String,
         default: ''
       },
+
+      /**
+       * Sets a Vue transition property on the `.dropdown-menu`. vue-select
+       * does not include CSS for transitions, you'll need to add them yourself.
+       * @type {String}
+       */
       transition: {
         type: String,
         default: 'expand'
       },
+
+      /**
+       * Enables/disables clearing the search text when an option is selected.
+       * @type {Boolean}
+       */
       clearSearchOnSelect: {
         type: Boolean,
         default: true
-      }
+      },
+
+      /**
+       * Tells vue-select what key to use when generating option
+       * labels when each `option` is an object.
+       * @type {String}
+       */
+      label: {
+        type: String,
+        default: 'label'
+      },
+
+      /**
+       * An optional callback function that is called each time the selected
+       * value(s) change. When integrating with Vuex, use this callback to trigger
+       * an action, rather than using :value.sync to retreive the selected value.
+       * @type {Function}
+       * @default {null}
+       */
+      onChange: Function
     },
 
     data() {
@@ -195,6 +262,9 @@
     },
 
     watch: {
+      value(val, old) {
+        this.onChange && val !== old ? this.onChange(val) : null
+      },
       options() {
         this.$set('value', this.multiple ? [] : null)
       },
@@ -203,10 +273,16 @@
       },
       filteredOptions() {
         this.typeAheadPointer = 0;
-      }
+      },
     },
 
     methods: {
+
+      /**
+       * Select a given option.
+       * @param  {Object||String} option
+       * @return {void}
+       */
       select(option) {
           if (! this.isOptionSelected(option) ) {
             if (this.multiple) {
@@ -226,6 +302,15 @@
             }
           }
 
+          this.onAfterSelect(option)
+      },
+
+      /**
+       * Called from this.select after each selection.
+       * @param  {Object||String} option
+       * @return {void}
+       */
+      onAfterSelect(option) {
           if (!this.multiple) {
             this.open = !this.open
           }
@@ -233,8 +318,17 @@
           if( this.clearSearchOnSelect ) {
             this.search = ''
           }
+
+          // if( this.onChange ) {
+          //   this.onChange(this.$get('value'))
+          // }
       },
 
+      /**
+       * Toggle the visibility of the dropdown menu.
+       * @param  {Event} e
+       * @return {void}
+       */
       toggleDropdown( e ) {
         if( e.target === this.$els.openIndicator || e.target === this.$els.search || e.target === this.$els.toggle || e.target === this.$el ) {
           if( this.open ) {
@@ -246,6 +340,11 @@
         }
       },
 
+      /**
+       * Check if the given option is currently selected.
+       * @param  {Object||String}  option
+       * @return {Boolean}         True when selected || False otherwise
+       */
       isOptionSelected( option ) {
         if( this.multiple && this.value ) {
           return this.value.indexOf(option) !== -1
@@ -254,6 +353,12 @@
         return this.value === option;
       },
 
+      /**
+       * If the selected option has option['value'] return it.
+       * Otherwise, return the entire option.
+       * @param  {Object||String} option
+       * @return {Object||String}
+       */
       getOptionValue( option ) {
         if( typeof option === 'object' && option.value ) {
           return option.value;
@@ -262,22 +367,47 @@
         return option;
       },
 
+      /**
+       * Generate the option label text. If {option}
+       * is an object, return option[this.label].
+       *
+       * @param  {Object || String} option
+       * @return {String}
+       */
       getOptionLabel( option ) {
-        if( typeof option === 'object' && option.label ) {
-          return option.label;
+        if( typeof option === 'object' ) {
+          if( this.label && option[this.label] ) {
+            return option[this.label];
+          } else if( option.label ) {
+            return option.label
+          }
         }
-
         return option;
       },
 
+      /**
+       * Move the typeAheadPointer visually up the list by
+       * subtracting the current index by one.
+       * @return {void}
+       */
       typeAheadUp() {
         if (this.typeAheadPointer > 0) this.typeAheadPointer--
       },
 
+      /**
+       * Move the typeAheadPointer visually down the list by
+       * adding the current index by one.
+       * @return {void}
+       */
       typeAheadDown() {
         if (this.typeAheadPointer < this.filteredOptions.length - 1) this.typeAheadPointer++
       },
 
+      /**
+       * Select the option at the current typeAheadPointer position.
+       * Optionally clear the search input on selection.
+       * @return {void}
+       */
       typeAheadSelect() {
         if( this.filteredOptions[ this.typeAheadPointer ] ) {
           this.select( this.filteredOptions[ this.typeAheadPointer ] );
@@ -288,6 +418,11 @@
         }
       },
 
+      /**
+       * If there is any text in the search input, remove it.
+       * Otherwise, blur the search input to close the dropdown.
+       * @return {[type]} [description]
+       */
       onEscape() {
         if( ! this.search.length ) {
           this.$els.search.blur()
@@ -296,6 +431,11 @@
         }
       },
 
+      /**
+       * Delete the value on Delete keypress when there is no
+       * text in the search input, & there's tags to delete
+       * @return {this.value}
+       */
       maybeDeleteValue() {
         if( ! this.$els.search.value.length && this.value ) {
           return this.multiple ? this.value.pop() : this.$set('value', null)
@@ -304,31 +444,57 @@
     },
 
     computed: {
-      cssClasses() {
+
+      /**
+       * Classes to be output on .dropdown
+       * @return {Object}
+       */
+      dropdownClasses() {
         return {
           open: this.open,
           searchable: this.searchable
         }
       },
 
+      /**
+       * Return the placeholder string if it's set
+       * & there is no value selected.
+       * @return {String} Placeholder text
+       */
       searchPlaceholder() {
         if( this.isValueEmpty && this.placeholder ) {
           return this.placeholder;
         }
       },
 
+      /**
+       * The currently available options, filtered
+       * by the search elements value.
+       * @return {[type]} [description]
+       */
       filteredOptions() {
         return this.$options.filters.filterBy(this.options, this.search)
       },
 
+      /**
+       * Check if there aren't any options selected.
+       * @return {Boolean}
+       */
       isValueEmpty() {
         if( this.value ) {
+          if( typeof this.value === 'object' ) {
+            return ! Object.keys(this.value).length
+          }
           return ! this.value.length
         }
 
         return true;
       },
 
+      /**
+       * Return the current value in array format.
+       * @return {Array}
+       */
       valueAsArray() {
         if( this.multiple ) {
           return this.value
