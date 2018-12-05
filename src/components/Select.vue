cd9eb287d0bd34f27e4cff007d260dbd61b3be31
cooperateDev <pavelasanov1206@gmail.com>
2018-12-05 19:11:14 +0100

    import vSelected from 'vue-select' (#707)

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -975,24 +975,9 @@
           if (this.clearSearchOnBlur) {
             this.search = ''
           }
-          this.closeSearchOptions()
-          return
+          this.open = false
+          this.$emit('search:blur')
         }
-        // Fixed bug where no-options message could not be closed
-        if(this.search.length === 0 && this.options.length === 0){
-          this.closeSearchOptions()
-          return
-        }
-      },
-
-      /**
-       * 'Private' function to close the search options
-       * @emits  {search:blur}
-       * @returns {void}
-       */
-      closeSearchOptions(){
-        this.open = false
-        this.$emit('search:blur')
       },
 
       /**
